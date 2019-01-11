5faa0b04d71e64134cb662d7e9165ce00fca47c5
Rob Corey <rcorey@metromile.com>
2019-01-11 16:50:52 -0500

    sanic

--- main.js
+++
@@ -1,9 +0,0 @@
-setInterval(function() {
-    if ($('#olive').hasClass('wobble-left')) {
-        $('#olive').addClass('wobble-right');
-        $('#olive').removeClass('wobble-left');
-    } else {
-        $('#olive').addClass('wobble-left');
-        $('#olive').removeClass('wobble-right');
-    }
-}, 1000);
